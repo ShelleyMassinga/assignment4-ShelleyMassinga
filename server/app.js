7d91c35df88d1389d166dcbd69b321fb36c7873c
ShelleyMassinga <shelley.massinga@gmail.com>
2024-11-03 16:09:10 -0500

    changed port from 80 to 8080

--- server/app.js
+++ server/app.js
@@ -55,7 +55,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
