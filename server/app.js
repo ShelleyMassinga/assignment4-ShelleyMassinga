fba84b01d34e67e2f7fbf5ebe65e08284974aa9f
ShelleyMassinga <shelley.massinga@gmail.com>
2024-11-03 13:37:26 -0500

    moved the build folder from client-side to server-side

--- server/app.js
+++ server/app.js
@@ -35,6 +35,10 @@ app.use('/products', productRoutes);
 app.use('/cart', cartRoutes);
 app.use('/wishlist', wishlistRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ server/public/build/precache-manifest.6803e965c44d0e2b21694e4a6a289550.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612812df68ae61a35f0f672b9d12ccb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bd136cf85f4f9a0234c",
+    "url": "/static/css/main.5205b81f.chunk.css"
+  },
+  {
+    "revision": "8ba9684e99f9cc07be1d",
+    "url": "/static/js/2.b0d02c28.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "7bd136cf85f4f9a0234c",
+    "url": "/static/js/main.274ca69c.chunk.js"
+  },
+  {
+    "revision": "a50475a1f486eef27c5a",
+    "url": "/static/js/runtime~main.4414e1f1.js"
+  }
+]);
